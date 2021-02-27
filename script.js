7f68bc4657c83fd1e02a98f8deb741776ac4498b
Максим <41415269+KemtaTam@users.noreply.github.com>
2021-02-27 15:04:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 const form = document.getElementById("form");
-let inputSeq = document.getElementsByTagName("strong")[0].innerHTML;
+let inputSeq = document.getElementsByTagName("strong")[0];
 let tbody = document.getElementsByTagName("tbody")[0];
 
-function buildAnswer()
-{
-	let readyAns="";
-	let state = 1;
-	let sumOfinputChar = tbody.children.length-1;
+//Объект, включающий в себя последовательности
+let CollOfIputSeq = {};
 
-	//объект, содержаий в себе ключ-значение входного символа и его номера
-	let symbolID = {};
-	for(j=0; j<sumOfinputChar; j++){
-		symbolID[String.fromCharCode(97+j)] = j+1;
-	}
-
-	//проход по входной последовательности
-	for(i=0; i<inputSeq.length; i++)
-	{
-		//читаю значение ячейки
-		let val = tbody.children[symbolID[inputSeq[i]]].children[state].innerHTML;
-		//сохраняю следуюoие состояние
-		state = val[0];
-		//формирую ответ
-		readyAns += val[2];
-	}
-
-	return readyAns;
+//Заполняю объект i случайными последовательностями
+for(let i=0; i<4; i++){
+	CollOfIputSeq[i] = getRandomSeq();
 }
-console.log(buildAnswer()); 
+
+//Количество переменных в объекте
+let numOfEl = Object.keys(CollOfIputSeq).length;
+
+let numOfseq=0;	//индекс, ходящий по последовательностям
+inputSeq.textContent = CollOfIputSeq[numOfseq];
+let inputSeqVal = inputSeq.innerHTML;
+
+//показать все ответы
+for(let i=0; i<numOfEl; i++)
+{
+	console.log(buildAnswer(CollOfIputSeq[i]));
+}
 
 //при нажатии на кнопку отправить ответ
-function retrieveFormValue(event) 
-{
+function retrieveFormValue(event) {
 	event.preventDefault(); //отправлять на сервер не нужно
 	valOfseq = form.querySelector('[name="output_sequence"]');
 
@@ -41,19 +34,50 @@ function retrieveFormValue(event)
 
 	console.log("v1", value);
 
-	let answer = buildAnswer();
-	
-	if (valOfseq.value == answer) alert("Correct answer");
-	else if(checkForm(form)) alert("Incorrect answer");
+	let answer = buildAnswer(inputSeqVal);
+
+	if (valOfseq.value == answer) {
+		alert("Correct answer");
+		numOfseq++; //при правильном ответе идет другая последовательность
+		inputSeq.textContent = CollOfIputSeq[numOfseq];
+		inputSeqVal = inputSeq.innerHTML;
+		if (numOfseq == numOfEl) alert("GOOOOOD!!!!");	//Если все ответы правильны
+	}
+	else if (checkForm(form)) alert("Incorrect answer! Try it again.");
 }
 form.addEventListener("submit", retrieveFormValue);
 
+//-------------------------------------------------------------------------------//
 
+//функция построения ответа
+function buildAnswer(inputSeqVal) {
+  let readyAns = "";
+  let state = 1;
+  let sumOfinputChar = tbody.children.length - 1;
+
+  //объект, содержаий в себе ключ-значение входного символа и его номера
+  let symbolID = {};
+  for (let i = 0; i < sumOfinputChar; i++) {
+    symbolID[String.fromCharCode(97 + i)] = i + 1;
+  }
+
+  //проход по входной последовательности
+  for (let i = 0; i < inputSeqVal.length; i++) {
+    //читаю значение ячейки
+    let val = tbody.children[symbolID[inputSeqVal[i]]].children[state].innerHTML;
+    //сохраняю следуюoие состояние
+    state = val[0];
+    //формирую ответ
+    readyAns += val[2];
+  }
+
+  return readyAns;
+}
 
 //функция проверки формы на корректность 
 function checkForm(form){
-	var e = 0;
-	for (var i = 0; i < form.length - 1; i++) 
+	let e = 0;
+	for (let i = 0; i < form.length - 1; i++) 
 	{
 		if (!form[i].value.replace(/^\s+|\s+$/g, "")) 
 		{
@@ -67,3 +91,20 @@ function checkForm(form){
 		return false;
 	} else return true;
 }
+
+//Функция, получающая случайную последовательность случайной длины
+function getRandomSeq(){
+	let test = "";
+	let randInd = getRandomIntInclusive(5,7);
+	for(let i=0; i<randInd; i++){
+		test += String.fromCharCode(97 + getRandomIntInclusive(0, tbody.children.length-2))
+	}
+	return test;
+}
+
+//возвращает рандомное число от min до max
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
